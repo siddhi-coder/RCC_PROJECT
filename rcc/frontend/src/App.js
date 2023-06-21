d5224ed3fd8483317ce3c6dbb4c7d07c316c1977
Siddhi gada <83527318+siddhi-coder@users.noreply.github.com>
2023-06-21 09:34:56 +0000

    first commit

---
+++ rcc/frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href = "/">RCC</a>
+      </header>
+      <main>
+        List Products 
+      </main>
+    </div>
+  );
+}
+
+export default App;
