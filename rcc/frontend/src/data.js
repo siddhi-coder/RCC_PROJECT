c38f87624986024aaaf23acbacd6b3c800f3e79a
Siddhi gada <83527318+siddhi-coder@users.noreply.github.com>
2023-06-21 09:57:49 +0000

    commmit

---
+++ rcc/frontend/src/data.js
@@ -0,0 +1,11 @@
+const data = {
+    products : [
+        {
+            name : "Chocolate Bar",
+            slug:"chocolate-bar",
+            category:"chocolate",
+            image:""
+        }
+    ]
+
+}
